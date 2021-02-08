e5ffac160d4ef2b89e178316c5cf9bfad44cce21
terraformtest09 <terraformtest09@gmail.com>
2021-02-08 13:59:37 +0000

    Deleted lambda

--- AutoTagging-Terraform/lambda/dead_letter_queue.tf
+++
@@ -1,5 +0,0 @@
-module "dead-letter-queue" {
-  source = "../sqs-queue"
-  name   = "${var.name}-dead-letter-queue"
-  tags   = var.tags
-}
--- AutoTagging-Terraform/lambda/module.tf
+++
@@ -1,5 +0,0 @@
-# module "vpc" {
-# #   count  = var.vpc_name == null ? 0 : 1
-#   source = "../vpc/data"
-#   name   = var.vpc_name
-# }
